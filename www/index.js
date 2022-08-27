b12cf44ea19e849e00a922f35c2e29ae1dd33f17
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2022-08-27 12:52:59 -0700

    makes a pretty picture a least

---
+++ www/index.js
@@ -0,0 +1,119 @@
+import { LitElement, css, html } from 'https://cdn.skypack.dev/lit';
+
+const rightKeys = ['d', 'ArrowRight'];
+const leftKeys = ['a', 'ArrowLeft'];
+
+class MovementController {
+	host;
+	direction = 0;
+
+	_onKeyDown = (event) => {
+		console.log('👇', event.key);
+		event.preventDefault();
+
+		if (rightKeys.includes(event.key) && this.direction == 0) {
+			this.direction = 1;
+			this.host.requestUpdate();
+		} else if (leftKeys.includes(event.key) && this.direction == 0) {
+			this.direction = -1;
+			this.host.requestUpdate();
+		}
+	};
+
+	_onKeyUp = (event) => {
+		event.preventDefault();
+		console.log('☝️', event.key);
+		if (
+			(rightKeys.includes(event.key) && this.direction == 1) ||
+			(leftKeys.includes(event.key) && this.direction == -1)
+		) {
+			this.direction = 0;
+			this.host.requestUpdate();
+		}
+	};
+
+	constructor(host) {
+		this.host = host;
+		host.addController(this);
+	}
+
+	hostConnected() {
+		window.addEventListener('keyup', this._onKeyUp);
+		window.addEventListener('keydown', this._onKeyDown);
+		// todo: listen for pointer events
+	}
+
+	hostDisconnected() {
+		window.removeEventListener('keyup', this._onKeyUp);
+		window.removeEventListener('keydown', this._onKeyDown);
+	}
+}
+
+class GameElement extends LitElement {
+	moving = new MovementController(this);
+
+	constructor() {
+		super();
+	}
+
+	static styles = css`
+		:host {
+			align-items: center;
+			background: linear-gradient(0deg, DarkOliveGreen 49vh, SkyBlue 50vh);
+			display: flex;
+			height: 100vh;
+			overflow: scroll;
+			position: relative;
+			white-space: nowrap;
+			width: 100vw;
+		}
+	`;
+
+	render() {
+		return html`
+			${this.moving.direction}
+			<slot name="player"></slot>
+			<slot></slot>
+		`;
+	}
+}
+
+class ThingElement extends LitElement {
+	constructor() {
+		super();
+	}
+	static styles = css`
+		:host {
+			line-height: 100vh;
+			font-size: 10vw;
+			text-shadow: 0 0.05em 0.1em hsl(100deg 50% 10% / 50%);
+		}
+	`;
+	render() {
+		return html`<span>
+			<slot></slot>
+		</span>`;
+	}
+}
+
+class PlayerElement extends ThingElement {
+	constructor() {
+		super();
+	}
+	static styles = [
+		ThingElement.styles,
+		css`
+			:host {
+				position: absolute;
+			}
+		`,
+	];
+	render() {
+		return html`<span>🙂</span>`;
+	}
+}
+
+// define custom elements
+customElements.define('game-be', GameElement);
+customElements.define('player-be', PlayerElement);
+customElements.define('thing-be', ThingElement);
