d64c12186fd8a9676876c95b7f1dbf4fd1dfc7da
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2023-08-14 13:39:59 -0700

    player changes facing direction

--- 13thCentury/lib.js
+++ 13thCentury/lib.js
@@ -1,15 +1,15 @@
 /** conditionally performs a view transition
- * @param {function} render - performs the DOM manipulation
+ * @param {function} manipulate - performs the DOM manipulation
  */
-export async function doTransition(render) {
+export async function doTransition(manipulate) {
 	/** @see https://developer.mozilla.org/en-US/docs/Web/API/ViewTransition */
 	if (document.startViewTransition) {
 		await document.startViewTransition(() => {
 			//console.debug('view transition starting');
-			render();
+			manipulate();
 		}).finished;
 	} else {
 		//console.debug('view transition not supported');
-		render();
+		manipulate();
 	}
 }
